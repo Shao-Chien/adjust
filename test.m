836c6b6c81271bbece5f31696f56af5263e52149
Shao-Chien <magicdebater@gmail.com>
2015-10-10 15:52:45 +0800

    seems work
    but some data is missing

--- test.m
+++ test.m
@@ -2,13 +2,13 @@
 [numc,txtc,rawc]=xlsread('div_cash.xlsx');
 [nums,txts,raws]=xlsread('div_stock.xlsx');
 num_adjust = num;
-jc = 2;
-js = 2;
 n=1;
 %for n = 1:1590 
+jc = 2;
+js = 2;
 for i=3:990
      if jc<=7
-     if isequal(raw(i,n),rawc(jc,n+1))>0 %加回股利
+     if isequal(raw(i,1),rawc(jc,n+1))>0 %加回股利
          jc = jc+1; % 股利位置
          for k = i:990
              num_adjust(k-3,n) = num_adjust(k-3,n) + numc(jc,n);%加回股利
@@ -16,10 +16,10 @@ for i=3:990
          jc = jc+1;
      end
      end
-     if isequal(raw(i,n),raws(js,n+1))>0 %股票股利還原
+     if isequal(raw(i,1),raws(js,n+1))>0 %股票股利還原
          js = js+1;%股票股利位置
          for k = i:990
-             num_adjust(k-3,n) = numadhust(k-3,n)*(1+nums(js,n)/10);
+             num_adjust(k-3,n) = num_adjust(k-3,n)*(1+nums(js,n)/10);
          end
          js = js+1;
          if js>=7
@@ -27,6 +27,7 @@ for i=3:990
          end
      end
 end
+num_adjust(:,n)-num(:,n);
 %end
- num_adjust(:,1)-num(:,1)
+
  
